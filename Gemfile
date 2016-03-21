8f93b3cfc672eea4c6edbcf7eca4d10e0a8a574d
손병우 <ByeongWoo@gimjaeyeong-ui-MacBook-Air.local>
2016-03-21 20:10:37 +0900

    admin page 생성

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'rails_admin'
 gem 'carrierwave'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.1'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+
+  validates :name, presence: true
+  belongs_to :player
+
+  rails_admin do
+    configure :player do
+      label 'Owner of this ball: '
+    end
+  end
 end
