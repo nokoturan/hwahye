7cda980b9ee5b563112a207f497c0bbc15734511
손병우 <ByeongWoo@gimjaeyeong-ui-MacBook-Air.local>
2016-03-22 01:35:32 +0900

    자유게시판 생성

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,3 @@
 class Post < ActiveRecord::Base
   mount_uploader :image, ImageUploader
-
-  validates :name, presence: true
-  belongs_to :player
-
-  rails_admin do
-    configure :player do
-      label 'Owner of this ball: '
-    end
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315035039) do
+ActiveRecord::Schema.define(version: 20160321111353) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
